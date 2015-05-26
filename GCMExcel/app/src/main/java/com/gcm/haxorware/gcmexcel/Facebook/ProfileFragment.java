0c87fe9f156888ad6b5dd7ef3d2a45de942898d8
doylefermi <doylefermi@gmail.com>
2015-05-26 08:47:19 +0530

    Test FB
    Signed-off-by: doylefermi <doylefermi@gmail.com>

---
+++ GCMExcel/app/src/main/java/com/gcm/haxorware/gcmexcel/Facebook/ProfileFragment.java
@@ -0,0 +1,250 @@
+package com.gcm.haxorware.gcmexcel.Facebook;
+
+/**
+ * Created by haxorware on 25/5/15.
+ */
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.gcm.haxorware.gcmexcel.MainActivity;
+import com.gcm.haxorware.gcmexcel.R;
+import com.github.gorbin.asne.core.SocialNetwork;
+import com.github.gorbin.asne.core.listener.OnPostingCompleteListener;
+import com.github.gorbin.asne.core.listener.OnRequestSocialPersonCompleteListener;
+import com.github.gorbin.asne.core.persons.SocialPerson;
+import com.github.gorbin.asne.facebook.FacebookSocialNetwork;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Random;
+
+public class ProfileFragment extends Fragment implements OnRequestSocialPersonCompleteListener {
+    private String message = "Need simple social networks integration? Check this lbrary:";
+    private String TAG = "GCM";
+
+    private static final String NETWORK_ID = "NETWORK_ID";
+    private SocialNetwork socialNetwork;
+    private int networkId;
+    private ImageView photo;
+    private TextView name;
+    private TextView id;
+    private TextView info;
+    private Button friends;
+    private Button share;
+    private RelativeLayout frame;
+
+    public static ProfileFragment newInstannce(int id) {
+        ProfileFragment fragment = new ProfileFragment();
+        Bundle args = new Bundle();
+        args.putInt(NETWORK_ID, id);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ProfileFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        networkId = getArguments().containsKey(NETWORK_ID) ? getArguments().getInt(NETWORK_ID) : 0;
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle("Profile");
+
+        View rootView = inflater.inflate(R.layout.profile_fragment, container, false);
+
+        frame = (RelativeLayout) rootView.findViewById(R.id.frame);
+        photo = (ImageView) rootView.findViewById(R.id.imageView);
+        name = (TextView) rootView.findViewById(R.id.name);
+        id = (TextView) rootView.findViewById(R.id.id);
+        info = (TextView) rootView.findViewById(R.id.info);
+        /*friends = (Button) rootView.findViewById(R.id.friends);
+        friends.setOnClickListener(friendsClick);
+        share = (Button) rootView.findViewById(R.id.share);
+        share.setOnClickListener(shareClick);*/
+        colorProfile(networkId);
+
+        socialNetwork = MainFragment.mSocialNetworkManager.getSocialNetwork(networkId);
+        socialNetwork.setOnRequestCurrentPersonCompleteListener(this);
+        socialNetwork.requestCurrentPerson();
+
+        MainActivity.showProgress("Loading social person");
+        return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_main, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                socialNetwork.logout();
+                getActivity().getSupportFragmentManager().popBackStack();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onRequestSocialPersonSuccess(int i, SocialPerson socialPerson) {
+        String fbid="";
+        String fbname="";
+        String fbinfo="";
+        String gcmid="";
+
+        SharedPreferences.Editor editor = MainActivity.preferences.edit();
+        editor.putString("id",socialPerson.id);
+        editor.putString("name",socialPerson.name);
+        editor.putString("info",socialPerson.toString());
+        editor.putString("gcmid","");
+
+        editor.apply();
+
+
+
+        fbid = MainActivity.preferences.getString("id", "");
+        fbname = MainActivity.preferences.getString("name", "");
+        fbinfo = MainActivity.preferences.getString("info", "");
+        gcmid = MainActivity.preferences.getString("gcmid", "");
+        if(!fbid.equalsIgnoreCase("") && gcmid=="")
+        {
+            Toast.makeText(getActivity(),"GCM Service Started - "+fbid, Toast.LENGTH_LONG).show();
+            startgcm(fbid,fbname,fbinfo,gcmid);
+
+        }
+        MainActivity.hideProgress();
+        name.setText(fbname);
+        id.setText(fbid);
+        String socialPersonString = fbinfo;
+        String infoString = socialPersonString.substring(socialPersonString.indexOf("{")+1, socialPersonString.lastIndexOf("}"));
+        info.setText(infoString.replace(", ", "\n"));
+        Picasso.with(getActivity())
+                .load(socialPerson.avatarURL)
+                .into(photo);
+    }
+
+    @Override
+    public void onError(int networkId, String requestID, String errorMessage, Object data) {
+        MainActivity.hideProgress();
+        Toast.makeText(getActivity(), "ERROR: " + errorMessage, Toast.LENGTH_LONG).show();
+    }
+
+  /*  private View.OnClickListener friendsClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            FriendsFragment friends = FriendsFragment.newInstannce(networkId);
+            getActivity().getSupportFragmentManager().beginTransaction()
+                    .addToBackStack("friends")
+                    .replace(R.id.container, friends)
+                    .commit();
+        }
+    };
+
+    private View.OnClickListener shareClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            AlertDialog.Builder ad = alertDialogInit("Would you like to post Link:", link);
+            ad.setPositiveButton("Post link", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    Bundle postParams = new Bundle();
+                    postParams.putString(SocialNetwork.BUNDLE_NAME, "Simple and easy way to add social networks for android application");
+                    postParams.putString(SocialNetwork.BUNDLE_LINK, link);
+
+                        socialNetwork.requestPostLink(postParams, message, postingComplete);
+
+                }
+            });
+            ad.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int i) {
+                    dialog.cancel();
+                }
+            });
+            ad.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                public void onCancel(DialogInterface dialog) {
+                    dialog.cancel();
+                }
+            });
+            ad.create().show();
+        }
+    };
+
+    private OnPostingCompleteListener postingComplete = new OnPostingCompleteListener() {
+        @Override
+        public void onPostSuccessfully(int socialNetworkID) {
+            Toast.makeText(getActivity(), "Sent", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onError(int socialNetworkID, String requestID, String errorMessage, Object data) {
+            Toast.makeText(getActivity(), "Error while sending: " + errorMessage, Toast.LENGTH_LONG).show();
+        }
+    };*/
+
+    private void colorProfile(int networkId){
+        int color = getResources().getColor(R.color.dark);
+        int image = R.mipmap.ic_launcher;
+        switch (networkId) {
+            case FacebookSocialNetwork.ID:
+                color = getResources().getColor(R.color.facebook);
+                image = R.drawable.com_facebook_profile_picture_blank_square;
+                break;
+        }
+        frame.setBackgroundColor(color);
+        name.setTextColor(color);
+      //  friends.setBackgroundColor(color);
+      //  share.setBackgroundColor(color);
+        photo.setBackgroundColor(color);
+        photo.setImageResource(image);
+    }
+
+    private AlertDialog.Builder alertDialogInit(String title, String message){
+        AlertDialog.Builder ad = new AlertDialog.Builder(getActivity());
+        ad.setTitle(title);
+        ad.setMessage(message);
+        ad.setCancelable(true);
+        return ad;
+    }
+
+    public void startgcm(String fbid, String fbname, String fbinfo,String gcmid ) {
+
+
+
+
+
+
+    }
+
+
+}
